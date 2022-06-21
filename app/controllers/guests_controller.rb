112b79522761864db9f29ee29d26c5d59c1bd733
anthonybchung <11941@coderacademy.edu.au>
2022-06-22 00:34:16 +1000

    teachers root, guests root

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,5 +1,10 @@
 class GuestsController < ApplicationController
   def index
+    unless current_user.nil?
+      if current_user.role == 2
+        redirect_to teachers_index_path
+      end  
+    end
   end
 
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'teachers/index'
 
   devise_for :users
   
