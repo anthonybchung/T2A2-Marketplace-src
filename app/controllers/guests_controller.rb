b056581ad789901a9d71fcf4eb477046033abd1d
anthonybchung <11941@coderacademy.edu.au>
2022-06-21 15:51:50 +1000

    create guests_controller and views base

---
+++ app/controllers/guests_controller.rb
@@ -0,0 +1,10 @@
+class GuestsController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def help
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'guests/index'
+  get 'guests/about'
+  get 'guests/help'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root "guests#index"
 end
