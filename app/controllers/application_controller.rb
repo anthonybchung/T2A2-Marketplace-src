81e6965f6b1e06d1b5bbd22b73db3c8012314bfc
anthonybchung <11941@coderacademy.edu.au>
2022-06-28 23:28:55 +1000

    add attributes to sign up form' :

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+        user_params.permit(:role,:email,:user_name,:password,:password_confirmation)
+      end
       devise_parameter_sanitizer.permit(:account_update) do |user_params|
         user_params.permit(:role, :email, :user_name, :password, :password_confirmation, :current_password)
       end
