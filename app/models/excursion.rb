d2c271fce749bb64e7e66f266eb19dc94addb0c0
anthonybchung <11941@coderacademy.edu.au>
2022-07-06 15:10:56 +1000

    enum excursion model

--- app/models/excursion.rb
+++ app/models/excursion.rb
@@ -9,4 +9,14 @@ class Excursion < ApplicationRecord
     has_many :passengers, through: :groups
 
     has_one_attached :image
+
+    enum state: {
+        ACT: 1,
+        QLD: 2,
+        NSW: 3,
+        NT: 4,
+        SA: 5,
+        TAS: 6,
+        WA: 7
+    }
 end
