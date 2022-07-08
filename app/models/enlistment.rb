9d34b5318187215731afa8556b93871fc5f86adc
anthonybchung <11941@coderacademy.edu.au>
2022-07-09 00:55:54 +1000

    remove unwanted model

--- app/models/enlistment.rb
+++
@@ -1,8 +0,0 @@
-class Enlistment < ApplicationRecord
-    validates :user_id, presence: true
-    validates :course_id, presence: true
-    #from the many-to-many relationship
-    # Enlist belongs to a user and an excursion.
-    belongs_to :user
-    belongs_to :excursion
-end
